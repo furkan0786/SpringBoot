b1ee23a74da41a50b0590827b7167d1838c16766
Furkan <furkanali0786@gmail.com>
2018-07-23 23:46:43 +0530

    first commit

---
+++ src/main/java/com/furkan/data/api/Topic/ControllerClass.java
@@ -0,0 +1,44 @@
+package com.furkan.data.api.Topic;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+public class ControllerClass {
+	@Autowired
+	private TopicService topicservice;
+	
+	
+	@RequestMapping("/topics")
+	public List<Topic> getAllTopics(){
+		return topicservice.getAllTopics();
+	}
+	@RequestMapping("/topics/{id}")
+	public Optional<Topic> getTopic(@PathVariable String id) {
+		return topicservice.getTopic(id);
+	}
+	@RequestMapping(method=RequestMethod.POST, value="/topics")
+	public void addTopic(@RequestBody Topic topic) {
+		topicservice.addTopic(topic);
+	}
+	@RequestMapping(method=RequestMethod.PUT, value="/topics/{id}")
+	public void updateTopic(@PathVariable String id,@RequestBody Topic topic) {
+		topicservice.updateTopic(id,topic);
+	}
+	@RequestMapping(method=RequestMethod.DELETE, value="/topics/{id}")
+	public void deleteTopic(@PathVariable String id) {
+		topicservice.deleteTopic(id);
+	}
+	
+}
---
+++ src/main/java/com/furkan/data/api/Topic/TopicRepository.java
@@ -0,0 +1,7 @@
+package com.furkan.data.api.Topic;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface TopicRepository extends CrudRepository<Topic,String>{
+
+}
